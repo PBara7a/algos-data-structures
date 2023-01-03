f3ed760a88e649499e7136a338270e2538dddf0f
Paulo Barata <pbara7a@gmail.com>
2023-01-03 15:47:35 +0000

    Dynamic array

--- static_&_dynamic_array.py
+++ static_&_dynamic_array.py
@@ -1,4 +1,3 @@
-
 class StaticArray:
     """A fixed-length sequence of references in contiguous memory."""
 
@@ -33,3 +32,22 @@ class StaticArray:
     def __str__(self) -> str:
         """Return a string representation of the array."""
         return str(self.items)
+
+class DynamicArray(StaticArray):
+    """An array that can grow and shrink."""
+
+    def resize(self, length: int) -> None:
+        """Shorten or extend the array to the new length.
+
+        Preconditions: 0 <= length; length != self.length()
+        Postconditions: if pre-self is a_0, a_1, ..., a_n then
+        post-self is b_0, b_1, ..., b_m with
+        - n == pre-self.length() - 1
+        - m == length - 1
+        - b_i == a_i for every i from 0 to min(n, m)
+        - b_i == None for every i from min(m, n) + 1 to m
+        """
+        new_array = [None] * length
+        for index in range(0, min(length, self.length())):
+            new_array[index] = self.items[index]
+        self.items = new_array
